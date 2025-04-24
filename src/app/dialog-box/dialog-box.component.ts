855bbc789717446885f037356d240ada55358118
Natasha <natashashanae94@gmail.com>
2025-04-24 13:24:38 -0400

    hooked up my firestore db to my frontend. Now will deploy and test this out

--- src/app/dialog-box/dialog-box.component.ts
+++ src/app/dialog-box/dialog-box.component.ts
@@ -1,8 +1,8 @@
-import { Component, inject } from '@angular/core';
-import { Firestore, collection, collectionData, addDoc} from '@angular/fire/firestore';
+import { Component, OnInit } from '@angular/core';
+import { Firestore, collection, collectionData, addDoc } from '@angular/fire/firestore';
 import { ApiService } from '../api.service';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 
 @Component({
@@ -11,16 +11,18 @@ import {MatInputModule} from '@angular/material/input';
   templateUrl: './dialog-box.component.html',
   styleUrl: './dialog-box.component.scss'
 })
-export class DialogBox { 
+export class DialogBox {
+
   //Calling API
   pageSpeedData: any;
-  
-  constructor(private apiService: ApiService) {}
-  
-    fetchPerformanceData(url: string): void {
-      this.apiService.getPageSpeedData(url).subscribe(
-        data => {
 
+  constructor(private apiService: ApiService, private firestore: Firestore) { }
+
+  fetchPerformanceData(url: string): void {
+    this.apiService.getPageSpeedData(url).subscribe({
+
+      next: async (data) => {
+        try {
           let pagespeedData = {
             id: data.id,
             timestamp: data.analysisUTCTimestamp,
@@ -34,16 +36,21 @@ export class DialogBox {
           this.pageSpeedData = data;
 
           console.log({
-            performanceData: pagespeedData, 
+            performanceData: pagespeedData,
 
             //Uncomment this statement to see the whole request JSON from API
             /* request: data*/
-          }); 
-          
-        },
-        error => {
-          console.error('Error fetching PageSpeed data', error);
+          });
+          const colRef = collection(this.firestore, 'posts');
+          const docRef = await addDoc(colRef, data);
+
+        } catch (error) {
+          console.error('Firestore write error:', error);
         }
-      );
-    }
+      },
+      error: (err) => {
+        console.error('Error fetching PageSpeed data', error);
+      }
+    });
+}
 }
