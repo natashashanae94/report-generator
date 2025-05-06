4d4a3e9fbc9e49e232c5282d41a3114e8168415f
Natasha <natashashanae94@gmail.com>
2025-05-05 22:22:40 -0400

    Trouble with ng build. Added a provider for firestore in the app.config.ts, but it's still throwing the same r3InjectorError. Will continue to debug later

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,18 +1,21 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, PLATFORM_ID, inject } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app.routes';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
-import { getDataConnect, provideDataConnect } from '@angular/fire/data-connect';
-import { connectorConfig } from '@firebasegen/default-connector';
-import { provideTanStackQuery, QueryClient } from '@tanstack/angular-query-experimental';
 import { environment } from '../environments/environment';
+
+const firestoreProvider = () => {
+  const platformId = inject(PLATFORM_ID);
+  return isPlatformBrowser(platformId) ? getFirestore() : ({} as any);
+};
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes), // Example: Configuring router providers
     provideHttpClient(), provideFirebaseApp(() => initializeApp(environment.firebase)),
-    provideFirestore(() => getFirestore()), 
+    provideFirestore(firestoreProvider), 
   ]
 };
